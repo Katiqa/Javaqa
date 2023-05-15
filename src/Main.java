b2761b067a9c29a0c37eae2c76f702bee458fab1
Kati <katyushka.zykova.2015@mail.ru>
2023-05-15 18:07:46 +0300

    Code

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Hi");
+
+        int eaters = 5; // сколько людей будут есть
+
+        int water = 3000; // миллилитров воды
+        int potatoes = 5; // картофелин
+        int chicken = 6; // куриных бёдер
+        int spices = 10; // ложек специй
+
+        System.out.println("Сварили суп. На одного человека вышло:");
+        System.out.println((water / eaters) + " миллилитров(а) воды");
+        System.out.println((potatoes / eaters) + " картофелин(а/ы)");
+        System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
+        System.out.println((spices / eaters) + " ложек(ки/ка) специй");
+
     }
 }
